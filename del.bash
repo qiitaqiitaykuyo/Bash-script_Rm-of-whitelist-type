8b27d6eeae49b4518cd81436c2a3d87e2fb10377
qiitaqiitaykuyo <127013044+qiitaqiitaykuyo@users.noreply.github.com>
2025-01-03 19:09:45 +0900

    Update del.bash

--- del.bash
+++ del.bash
@@ -1,35 +1,44 @@
 #!/bin/bash
 shopt -s expand_aliases
-alias del='set -f && DelEx'
+alias del='set -f && DelEx';
 function DelEx () {
     set +o noglob
     set -o nounset
-    ErrMsg="This function requires arguments."
-    Usage="Usage: del <path> [<name to exclude> <name to exclude> ...]"
-    ! (: ${1:?"${ErrMsg}${IFS}${Usage}${IFS}"}) && return 1
-    Path="$1"
+    local ErrMsg="This function requires arguments."
+    local Usage="Usage: del <path> [<name to exclude> <name to exclude> ...]"
+    ! (: ${1:?"${ErrMsg}${IFS}${Usage}${IFS}"}) && { set +u; return 1; }
+    set +o nounset
+    local Path="$1"
     Path="$(realpath -- "$Path")"
     [[ "$Path" == "/" ]] && {
         echo "Cannot execute.";
         return 1;
     }
-    unset Arg Args
-    N=2; Arg=(); Args=()
+    local N=2 Arg=() Escaped Args=()
     while [[ "$N" -le "$#" ]]; do
         Arg[$N]="$N"
         Escaped=$(perl -e "print quotemeta('${!Arg[$N]}');")
         Args+=(! -regex ".*/${Escaped}\(/.*\|\)")
        ((N++))
     done
+    echo "[Summary]"
+    echo -e "Target path: \n  '$Path'"
     [[ -d $Path ]] && {
         [[ $1 =~ ^\.+(/+\.*)*$ ]] || { find "$Path" -xdev -maxdepth 0 -delete 2>/dev/null && return; }
-        find "$Path" -xdev -depth ! -path "$Path" -regextype findutils-default "${Args[@]}" -delete 2> >(grep -v "Directory not empty" >&2);
+        echo "Name to exclude: ";
+        local width Nest;
+        width=$((`tput cols`-`tput cols`/3));
+        Nest=("${Arg[@]/#/\$\{}");
+        eval echo "${Nest[@]/%/@Q\}}" | fold -s -w $width | awk '{print "  " $0}';
+        echo -e "\n[Verbose]";
+        find "$Path" -xdev -depth ! -path "$Path" -regextype findutils-default "${Args[@]}" -print -delete;
         return;
     }
     [[ -f $Path ]] && {
         find "$Path" -xdev -maxdepth 0 -delete;
         return;
     }
+    echo "failed."
     return 255
 }
 
